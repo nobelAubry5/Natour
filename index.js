23f132be2ab58d18dd2c1f49eb26af25837c231a
Aubry Nobel Niyonkuru <nobelaubry5@gmail.com>
2024-04-29 19:46:09 -0400

    GET request for tours finished

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', app: 'Natours' });
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can post to this endpoint');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours: tours },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.status(200).send('You can post to this endpoint');
-});
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
