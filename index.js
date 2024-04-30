1f8e318778f2069732f64685b10811b1d9a98214
Aubry Nobel Niyonkuru <nobelaubry5@gmail.com>
2024-04-30 10:59:16 -0400

    Post-Patch-Delete requests  finished

--- index.js
+++ index.js
@@ -2,26 +2,73 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
-
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.status(200).send('You can post to this endpoint');
-// });
+// Middleware
+app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-app.get('/api/v1/tours', (req, res) => {
+
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: { tours: tours },
   });
-});
+};
+const getTour = (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour || id > tours.length) {
+    return res.status(404).json({ status: 'Fail', message: 'Invalid tour id' });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { tour: tour },
+  });
+};
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour },
+      });
+    }
+  );
+};
+
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'Fail', message: 'Invalid tour id' });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { tour: 'Updated tour ...' },
+  });
+};
+
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'Fail', message: 'Invalid tour id' });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
