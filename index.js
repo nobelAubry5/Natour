67671e9b7407e348f8130dbd19904b0c8db656d7
Aubry Nobel Niyonkuru <nobelaubry5@gmail.com>
2024-05-01 08:47:25 -0400

    Environment variables settings done

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
 // Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 // Access html,css and img files in the folder public
 app.use(express.static(`${__dirname}/public`));
